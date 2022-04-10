31f2f9bb8907861fe17e6887a837d60e9ccd02a7
Hao Le <51278143+anhhao135@users.noreply.github.com>
2022-04-10 00:44:08 -0700

    updated for 2022 april

--- about.php
+++ about.php
@@ -130,11 +130,16 @@ a:hover, a:active {
  <main style="text-align:center;">
 
 
-   <div id="about-container" style = "height:auto;">
+  <div id="about-container" style = "height:auto;">
 
-     <img id ="avatar" src="img/about-2.jpg">
+    
 
-      <div id = "about-text">Hello! Thanks for checking out my site. I don't get a lot of visitors so it's nice that you're here.<br><br>I'm a fourth year electrical engineering student at UC San Diego currently part of Dr. Truong Nguyen's Video Processing Lab. I minor in studio arts. If I'm not home tending my garden or fish tanks, I'm probably in my car driving to faraway places to paint. Occasionally, I write songs and play my guitar to get over my overthinking.</I><br><br>Contact me at:<br> anhhao135@gmail.com<br><br><a href="https://github.com/anhhao135/website.git">GitHub</a> for this whole site.<br><br>My <a href="resume.pdf">résumé</a>.<br>
+
+    
+
+     <img id ="avatar" src="img/aboutMe/collage.png">
+
+      <div id = "about-text">Hello! Thanks for checking out my site. I don't get a lot of visitors so it's nice that you're here.<br><br>I'm a fourth year electrical engineering student, currently working at <a href="https://www.quartus.com/">Quartus Engineering</a>. I minor in studio arts. If I'm not home tending my garden or fish tanks, I'm probably in my car driving to faraway places to paint. Occasionally, I write songs and play my guitar to get over my overthinking.</I><br><br>Contact me at:<br> anhhao135@gmail.com<br><br><a href="https://github.com/anhhao135/website.git">GitHub</a> for this whole site.<br><br>My <a href="resume.pdf">résumé</a>.<br>
 
         <nav id="links">
           <li><a href="https://www.linkedin.com/in/hao-le-07b726132/" class="fa fa-linkedin"></a></li>
--- boilerplate/footer.php
+++ boilerplate/footer.php
@@ -3,7 +3,7 @@
 
 <div>
 
-<footer>Hao Le, 2021. Thanks for stopping by!</footer>
+<footer>Hao Le, 2022. Thanks for stopping by!</footer>
 
 </div>
 
