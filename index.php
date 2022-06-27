c07cc523f5c0ac24183c74487729aeedf3f04087
Hao Le <51278143+anhhao135@users.noreply.github.com>
2022-06-26 23:26:23 -0700

    slower index slideshow time

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <script>
     $(function(){
         $('.fadein img:gt(0)').hide();
-        setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
+        setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 5000);
     });
     </script>
 
